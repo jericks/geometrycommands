4d057464134ddb232c026f56f20426b1a54f6f6d
Jared Erickson <jared.erickson@gmail.com>
2012-01-16 17:17:05 -0800

    Add koch snowflake and siepinski carpet commands.

---
+++ src/main/java/org/geometrycommands/SierpinskiCarpetCommand.java
@@ -0,0 +1,75 @@
+package org.geometrycommands;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.shape.fractal.SierpinskiCarpetBuilder;
+import org.geometrycommands.SierpinskiCarpetCommand.SierpinskiCarpetOptions;
+import org.kohsuke.args4j.Option;
+
+/**
+ * A Command to create a Sierpinski Carpet Geometry.
+ * @author Jared Erickson
+ */
+public class SierpinskiCarpetCommand extends GeometryCommand<SierpinskiCarpetOptions> {
+
+    /**
+     * Get the command's name
+     * @return The command's name
+     */
+    @Override
+    public String getName() {
+        return "sierpinskicarpet";
+    }
+
+    /**
+     * Get the new SierpinskiCarpetOptions
+     * @return The new SierpinskiCarpetOptions
+     */
+    @Override
+    public SierpinskiCarpetOptions getOptions() {
+        return new SierpinskiCarpetOptions();
+    }
+
+    /**
+     * Create a Sierpinski carpet as a Geometry.
+     * @param geometry The input Geometry
+     * @param options The KochSnowflakeOptions
+     * @throws Exception if an error occurs
+     */
+    @Override
+    protected void processGeometry(Geometry geometry, SierpinskiCarpetOptions options) throws Exception {
+        SierpinskiCarpetBuilder builder = new SierpinskiCarpetBuilder(new GeometryFactory());
+        builder.setExtent(geometry.getEnvelopeInternal());
+        builder.setNumPoints(options.getNumberOfPoints());
+        Geometry outputGeometry = builder.getGeometry();
+        System.out.println(writeGeoemtry(outputGeometry, options));
+    }
+
+    /**
+     * The SierpinskiCarpetOptions
+     */
+    public static class SierpinskiCarpetOptions extends GeometryOptions {
+
+        /**
+         * The number of points.
+         */
+        @Option(name = "-n", usage = "The number of points.", required = true)
+        private int numberOfPoints;
+
+        /**
+         * Get the number of points.
+         * @return The number of points.
+         */
+        public int getNumberOfPoints() {
+            return numberOfPoints;
+        }
+
+        /**
+         * Set the number of points.
+         * @param numberOfPoints The number of points. 
+         */
+        public void setNumberOfPoints(int numberOfPoints) {
+            this.numberOfPoints = numberOfPoints;
+        }
+    }
+}
