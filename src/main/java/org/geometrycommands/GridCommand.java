07242a90874dcbc129b99d50d51d23f9cd965f96
Jared Erickson <jared.erickson@gmail.com>
2012-01-27 19:32:31 -0800

    Add grid command.

---
+++ src/main/java/org/geometrycommands/GridCommand.java
@@ -0,0 +1,135 @@
+package org.geometrycommands;
+
+import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import java.io.Reader;
+import java.io.Writer;
+import org.geometrycommands.GridCommand.GridOptions;
+import org.kohsuke.args4j.Option;
+
+/**
+ * A Command to create a vector grid 
+ * @author Jared Erickson
+ */
+public class GridCommand extends GeometryCommand<GridOptions> {
+
+    /**
+     * Get the command name
+     * @return The command name
+     */
+    @Override
+    public String getName() {
+        return "grid";
+    }
+
+    /**
+     * Get the GridOptions
+     * @return The GridOptions
+     */
+    @Override
+    public GridOptions getOptions() {
+        return new GridOptions();
+    }
+
+    /**
+     * Create a vector grid 
+     * @param geometry The input Geometry
+     * @param options The GeometryOptions
+     * @param reader The java.io.Reader
+     * @param writer The java.io.Writer
+     * @throws Exception if an error occurs
+     */
+    @Override
+    protected void processGeometry(Geometry geometry, GridOptions options, Reader reader, Writer writer) throws Exception {
+        
+        // Get the input Geometry's Envelope
+        Envelope env = geometry.getEnvelopeInternal();
+
+        // Extract the min X and Y
+        double minX = env.getMinX();
+        double minY = env.getMinY();
+
+        // Calculate hte envelop's width and height
+        double w = env.getWidth();
+        double h = env.getHeight();
+
+        // Get the number of columns and rows
+        int numberOfColumns = options.getNumberOfColumns();
+        int numberOfRows = options.getNumberOfRows();
+
+        // Calculate the cell width and height
+        double cellWidth = w / options.getNumberOfColumns();
+        double cellHeight = h / options.getNumberOfRows();
+
+        // Create the cells
+        GeometryFactory geometryFactory = new GeometryFactory();
+        Geometry[] geoms = new Geometry[numberOfRows * numberOfColumns];
+        int i = 0;
+        for (int r = 0; r < numberOfRows; r++) {
+            for (int c = 0; c < numberOfColumns; c++) {
+                double x = minX + (c * cellWidth);
+                double y = minY + (r * cellHeight);
+                Envelope e = new Envelope(x, x + cellWidth, y, y + cellHeight);
+                geoms[i] = geometryFactory.toGeometry(e);
+                i++;
+            }
+        }
+
+        // Create the Geometry grid
+        Geometry grid = geometryFactory.createGeometryCollection(geoms);
+        
+        // Write the Geometry grid
+        writer.write(writeGeometry(grid, options));
+    }
+
+    /**
+     * The GridOptions
+     */
+    public static class GridOptions extends GeometryOptions {
+
+        /**
+         * The number of columns
+         */
+        @Option(name = "-c", usage = "The number of columns", required = true)
+        private int numberOfColumns;
+
+        /**
+         * The number of rows
+         */
+        @Option(name = "-r", usage = "The number of rows", required = true)
+        private int numberOfRows;
+
+        /**
+         * Get the number of columns
+         * @return The number of columns
+         */
+        public int getNumberOfColumns() {
+            return numberOfColumns;
+        }
+
+        /**
+         * Set the number of columns
+         * @param numberOfColumns The number of columns 
+         */
+        public void setNumberOfColumns(int numberOfColumns) {
+            this.numberOfColumns = numberOfColumns;
+        }
+
+        /**
+         * Get the number of rows
+         * @return The number of rows
+         */
+        public int getNumberOfRows() {
+            return numberOfRows;
+        }
+
+        /**
+         * Set the number of rows
+         * @param numberOfRows The number of rows 
+         */
+        public void setNumberOfRows(int numberOfRows) {
+            this.numberOfRows = numberOfRows;
+        }
+    }
+}
---
+++ src/test/java/org/geometrycommands/GridCommandTest.java
@@ -0,0 +1,36 @@
+package org.geometrycommands;
+
+import org.geometrycommands.GridCommand.GridOptions;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * The GridCommand UnitTest
+ * @author Jared Erickson
+ */
+public class GridCommandTest {
+
+    @Test
+    public void execute() throws Exception {
+        String inputGeometry = "POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))";
+        GridOptions options = new GridOptions();
+        options.setGeometry(inputGeometry);
+        options.setNumberOfColumns(2);
+        options.setNumberOfRows(2);
+
+        Reader reader = new StringReader(inputGeometry);
+        StringWriter writer = new StringWriter();
+
+        GridCommand command = new GridCommand();
+        command.execute(options, reader, writer);
+        assertEquals("GEOMETRYCOLLECTION ("
+                + "POLYGON ((0 0, 0 5, 5 5, 5 0, 0 0)), "
+                + "POLYGON ((5 0, 5 5, 10 5, 10 0, 5 0)), "
+                + "POLYGON ((0 5, 0 10, 5 10, 5 5, 0 5)), "
+                + "POLYGON ((5 5, 5 10, 10 10, 10 5, 5 5)))",
+                writer.getBuffer().toString());
+    }
+}
