0b2542321b436a5c62a57c8c45c4f218ded69cf6
Jared Erickson <jared.erickson@gmail.com>
2013-09-14 19:24:16 -0700

    Sort names when listing them.

--- src/main/java/org/geometrycommands/ListCommand.java
+++ src/main/java/org/geometrycommands/ListCommand.java
@@ -2,17 +2,18 @@ package org.geometrycommands;
 
 import java.io.Reader;
 import java.io.Writer;
-import java.util.Iterator;
-import java.util.ServiceLoader;
+import java.util.*;
 
 /**
  * List all Commands
+ *
  * @author Jared Erickson
  */
 public class ListCommand implements Command<Options> {
 
     /**
      * Get the name of the command
+     *
      * @return The name of the command
      */
     @Override
@@ -22,6 +23,7 @@ public class ListCommand implements Command<Options> {
 
     /**
      * Get the description of what the Command does
+     *
      * @return The description of what the Command does
      */
     @Override
@@ -31,6 +33,7 @@ public class ListCommand implements Command<Options> {
 
     /**
      * Get a new Options
+     *
      * @return A new Options
      */
     @Override
@@ -40,19 +43,30 @@ public class ListCommand implements Command<Options> {
 
     /**
      * Get a List of all Geometry Commands
+     *
      * @param options The Options
-     * @param reader The java.io.Reader
-     * @param writer The java.io.Writer 
+     * @param reader  The java.io.Reader
+     * @param writer  The java.io.Writer
      * @throws Exception if an error occurs
      */
     @Override
     public void execute(Options options, Reader reader, Writer writer) throws Exception {
+        // Find all of the registered Commands
         final ServiceLoader<Command> commandServiceLoader = ServiceLoader.load(Command.class);
+        // Collect their names
+        List<String> names = new ArrayList<String>();
+        for (Iterator<Command> it = commandServiceLoader.iterator(); it.hasNext(); ) {
+            Command cmd = it.next();
+            names.add(cmd.getName());
+        }
+        // Sort the names
+        Collections.sort(names);
+        // Write the names
         String NEW_LINE = System.getProperty("line.separator");
         StringBuilder builder = new StringBuilder();
-        for (Iterator<Command> it = commandServiceLoader.iterator(); it.hasNext();) {
-            Command cmd = it.next();
-            builder.append(cmd.getName());
+        for (Iterator<String> it = names.iterator(); it.hasNext(); ) {
+            String name = it.next();
+            builder.append(name);
             if (it.hasNext()) {
                 builder.append(NEW_LINE);
             }
