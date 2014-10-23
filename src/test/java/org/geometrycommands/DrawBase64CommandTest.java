4b6ed205fa2ed70ad5972cfbb9ee1a66d1a4d20f
jericks <jared.erickson@gmail.com>
2014-10-22 18:04:20 -0700

    Change jpeg to png image types

--- src/test/java/org/geometrycommands/DrawBase64CommandTest.java
+++ src/test/java/org/geometrycommands/DrawBase64CommandTest.java
@@ -34,10 +34,10 @@ public class DrawBase64CommandTest extends BaseTest {
         assertTrue(result.length() > 0);
         assertFalse(result.startsWith("data:image/jpeg;base64"));
 
-        // JPEG with prefix
+        // PNG with prefix
         options = new DrawOptions();
         options.setGeometry(inputGeometry);
-        options.setType("jpeg");
+        options.setType("png");
         options.setIncludingPrefix(true);
 
         reader = new StringReader(inputGeometry);
@@ -47,7 +47,7 @@ public class DrawBase64CommandTest extends BaseTest {
         command.execute(options, reader, writer);
         result = writer.toString();
         assertTrue(result.length() > 0);
-        assertTrue(result.startsWith("data:image/jpeg;base64"));
+        assertTrue(result.startsWith("data:image/png;base64"));
     }
 
     @Test
@@ -58,6 +58,25 @@ public class DrawBase64CommandTest extends BaseTest {
                 "-g", "POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))"
         }, null);
         assertTrue(result.length() > 0);
-        assertFalse(result.startsWith("data:image/jpeg;base64"));
+        assertFalse(result.startsWith("data:image/png;base64"));
+
+        // PNG with prefix, geometry from input stream
+        result = runApp(new String[]{
+                "drawbase64",
+                "-y", "png",
+                "-p",
+                "-w", "300",
+                "-h", "250",
+                "-b", "0,0,0",
+                "-s", "255,255,255",
+                "-t", "0.75",
+                "-l", "255,255,0",
+                "-o", "0.55",
+                "-m", "square",
+                "-z", "12",
+                "-e", "0,0,10,10"
+        }, "POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))");
+        assertTrue(result.length() > 0);
+        assertTrue(result.startsWith("data:image/png;base64"));
     }
 }
