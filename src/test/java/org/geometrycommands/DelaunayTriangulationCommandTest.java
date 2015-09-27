67cf08e40c4ba9c9ff0040e46e5d6ebb94225921
jericks <jared.erickson@gmail.com>
2015-09-27 13:50:05 -0700

    Improve DelaunayTriangulationCommand test coverage

--- src/test/java/org/geometrycommands/DelaunayTriangulationCommandTest.java
+++ src/test/java/org/geometrycommands/DelaunayTriangulationCommandTest.java
@@ -25,7 +25,8 @@ public class DelaunayTriangulationCommandTest extends BaseTest {
                 + "(8.017822881853032 7.492806794533148))";
         DelaunayTriangulationOptions options = new DelaunayTriangulationOptions();
         options.setGeometry(inputGeometry);
-        
+        options.setConforming(false);
+
         Reader reader = new StringReader(inputGeometry);
         StringWriter writer = new StringWriter();
         
@@ -62,6 +63,23 @@ public class DelaunayTriangulationCommandTest extends BaseTest {
         // Geometry from input stream
         result = runApp(new String[]{
                 "delaunay",
+        }, "MULTIPOINT ((12.27256417862947 12.73833434783841), "
+                + "(13.737894633461437 7.658802439672621), "
+                + "(6.857126638942733 8.821305316892328), "
+                + "(9.260874914207697 13.087320259444919), "
+                + "(8.017822881853032 7.492806794533148))");
+        assertEquals("GEOMETRYCOLLECTION (POLYGON ((6.857126638942733 8.821305316892328, "
+                + "8.017822881853032 7.492806794533148, 9.260874914207697 13.087320259444919, "
+                + "6.857126638942733 8.821305316892328)), POLYGON ((9.260874914207697 "
+                + "13.087320259444919, 8.017822881853032 7.492806794533148, 12.27256417862947 "
+                + "12.73833434783841, 9.260874914207697 13.087320259444919)), POLYGON ((12.27256417862947 "
+                + "12.73833434783841, 8.017822881853032 7.492806794533148, 13.737894633461437 "
+                + "7.658802439672621, 12.27256417862947 12.73833434783841)))", result);
+
+        // Geometry from input stream
+        result = runApp(new String[]{
+                "delaunay",
+                "-c"
         }, "MULTIPOINT ((12.27256417862947 12.73833434783841), "
                 + "(13.737894633461437 7.658802439672621), "
                 + "(6.857126638942733 8.821305316892328), "
